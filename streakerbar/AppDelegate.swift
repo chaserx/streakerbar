49e9d5fa9555f37a442ad3e23ec41ddc01d17ba3
Chase Southard <chase.southard@gmail.com>
2015-03-23 22:33:18 -0400

    update title on interval w/ NSTimer; dummy data

--- streakerbar/AppDelegate.swift
+++ streakerbar/AppDelegate.swift
@@ -9,19 +9,21 @@
 // NEEDS:
 // - func: reads config possibly [github] from ~/.gitconfig or from NSUserDefaults
 // - func: pulls user contributions from https://github.com/users/:username/contributions
-// - func: updates statusItem.title on interval
+// - func: parse HTML/XML/SVG data from the contributions svg; grab data for today; update display
+// - func: updates statusItem.title on interval using NSTimer or GCD's dispatch_after
 // inspiration: https://github.com/akerl/githubstats/blob/master/lib/githubstats.rb
 
 
+
 import Cocoa
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var statusMenu: NSMenu!
     
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(30.0) // use -1 for variable length but it seems to get cut off on the right.
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         let icon = NSImage(named: "statusIcon")
@@ -29,6 +31,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         statusItem.image = icon
         statusItem.menu = statusMenu
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("randShit"), userInfo: nil, repeats: true)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
@@ -37,6 +40,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBAction func menuClicked(sender: NSMenuItem){
         NSApplication.sharedApplication().terminate(self)
+    }
+    
+    func updateTitle(title: String) {
+        statusItem.title = title
+    }
+    
+    func randShit() {
+        updateTitle(String(arc4random_uniform(10)))
     }
     
 }
